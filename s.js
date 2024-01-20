dc6b81c2a1fc7fe4461699e28cd3d098744c6ee7
Christinaliti <155884321+Christinaliti@users.noreply.github.com>
2024-01-20 22:59:22 +0100

    Update s.js

--- s.js
+++ s.js
@@ -375,7 +375,33 @@ function searchFunction() {
       }
     }
 }
-  
+ 
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let form = document.querySelector("#add-item-form");
+    form.addEventListener("submit", submitHandler);
+});
+         
+function submitHandler(event) {
+    event.preventDefault(); 
+    var imageUrl = document.getElementById('poster').value;
+    var description = document.getElementById('description').value;
+
+    
+    let img = document.createElement('img');
+    img.src = imageUrl;
+    
+    let desc = document.createElement('p');
+    desc.innerText = description;
+
+    let container = document.querySelector('.gallery');  
+    container.appendChild(img);
+    container.appendChild(desc);
+
+   
+}
+
+
   
   
   
